ef3c422f0102f80ed30cd9c72ce563a0c6bae4dc
suvobank <secretsubha09@gmail.com>
2022-04-22 13:04:49 +0530

    init

--- mynewsportal/src/App.js
+++ mynewsportal/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is test.
+          this is test
         </p>
         <a
           className="App-link"
